e3abbec09e2db494d8db2a0ec77802a4d2c7cd96
marcuss <marcus.sanchez@contractors.birchbox.com>
2018-05-31 15:08:54 -0500

    Initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
+library(matlib)
+
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## makeCacheMatrix will create a list containing the needed
+## functions to read and write the cache values of inverted matrix.
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinvmatrix <- function(mean) m <<- mean
+  getinvmatrix <- function() m
+  list(set = set, get = get,
+       setinvmatrix = setinvmatrix,
+       getinvmatrix = getinvmatrix)
 }
 
 
-## Write a short comment describing this function
-
+## this function solves the matrix asking makeCache if the solution is already cached.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+ ## Return a matrix that is the inverse of 'x'
+  m <- x$getinvmatrix()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinvmatrix(m)
 }
+
+#Simple test
+matrix <- makeCacheMatrix( matrix(c(1, -1/4,-1/4, 1),  nrow=2,  ncol=2))
+cacheSolve(matrix)
+matrix$getinvmatrix()
+matrix$get()
+
+
